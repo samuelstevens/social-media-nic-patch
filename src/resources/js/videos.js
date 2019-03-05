f1eca3e1dd6e01677fc7d01aa44fd89c42b0745d
Sam Stevens <stevens.994@osu.edu>
2019-03-04 22:22:57 -0500

    added video

--- src/resources/js/videos.js
+++ src/resources/js/videos.js
@@ -3,76 +3,51 @@
 // probably want to get this from a server somewhere
 // https://raw.githubusercontent.com/samuelstevens/social-media-nic-patch/master/api/videos/data.json
 
+var answers = [];
+
 var getData = function(callback) {
-  var request = new XMLHttpRequest();
-
-  request.onreadystatechange = function() {
-    if (request.readyState === 4) {
-      if (request.status === 200) {
-        if (callback) callback(request);
-      } else {
-        console.log("Error");
-      }
+  // var request = new XMLHttpRequest();
+  //
+  // request.onreadystatechange = function() {
+  //   if (request.readyState === 4) {
+  //     if (request.status === 200) {
+  //       if (callback) callback(JSON.parse(request.response););
+  //     } else {
+  //       console.log("Error");
+  //     }
+  //   }
+  // };
+  //
+  // request.open(
+  //   "GET",
+  //   "https://raw.githubusercontent.com/samuelstevens/social-media-nic-patch/master/api/videos/data.json"
+  // );
+  // request.send();
+  var responseJson = [
+    {
+      link: "https://www.youtube-nocookie.com/embed/MBRqu0YOH14",
+      channel: "Kurzgesagt",
+      title: "Optimistic Nihilism"
+    },
+    {
+      link: "https://www.youtube-nocookie.com/embed/sNhhvQGsMEc",
+      channel: "Kurzgesagt",
+      title: "The Fermi Paradox"
     }
-  };
+  ];
 
-  request.open(
-    "GET",
-    "https://raw.githubusercontent.com/samuelstevens/social-media-nic-patch/master/api/videos/data.json"
-  );
-  request.send();
-};
-
-var buildAnswer = function(answer, questionIndex, answerIndex) {
-  return (
-    '<div class="col-md-6 col-12"><div class="button button-sm" onclick="submitAnswer(' +
-    questionIndex +
-    ", " +
-    answerIndex +
-    ')">' +
-    answer +
-    "</div></div>"
-  );
-};
-
-var buildQuestion = function(problem, problemIndex) {
-  var html = "";
-
-  // html += "<li>";
-  html += '<p class="main-text">' + problem.question + '</p><div class="row">';
-  for (var i = 0; i < problem.answers.length; i++) {
-    html += buildAnswer(problem.answers[i], problemIndex, i);
-  }
-  html += "</div>";
-  // html += "</li>";
-
-  return html;
-};
-
-var buildHTML = function(feedback) {
-  var html = "";
-
-  for (var i = 0; i < feedback.length; i++) {
-    html += buildQuestion(feedback[i], i);
-  }
-
-  return html;
+  callback(responseJson);
 };
 
 var init = function() {
   getData(function(data) {
-    var json = JSON.parse(data.response);
-    var video = json[0];
+    // need to pick different index
+    var video = data[1];
+
     document.getElementById("video-player").src = video.link;
     document.getElementById("title").innerHTML =
       video.channel + " - " + video.title;
-    getData();
-    document.getElementById("questions").innerHTML = buildHTML(video.feedback);
   });
-};
-
-var submitAnswer = function(questionIndex, answerIndex) {
-  
 };
 
 if (typeof window.onload == "function") {
